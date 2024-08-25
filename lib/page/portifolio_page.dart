1c463bf64d842ececa17ee8bd681128ca28906d1
leonardovivo <leonardovivo3@gmail.com>
2024-08-24 23:36:27 -0300

    PortifolioPage preparado com os arquivos que serão criados

---
+++ lib/page/portifolio_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+// import 'package:seu_projeto/sections/section_intro.dart';
+// import 'package:seu_projeto/sections/section_about.dart';
+// import 'package:seu_projeto/sections/section_projects.dart';
+// import 'package:seu_projeto/sections/section_education.dart';
+// import 'package:seu_projeto/sections/section_contact.dart';
+
+class PortifolioPage extends StatelessWidget {
+  const PortifolioPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // SectionIntro(),
+            // SectionAbout(),
+            // SectionProjects(),
+            // SectionEducation(),
+            // SectionContact(),
+          ],
+        ),
+      ),
+    );
+  }
+}
