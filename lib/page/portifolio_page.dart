9a2f73758976078adb2967e1edea9cf2fa95ccbb
leonardovivo <leonardovivo3@gmail.com>
2024-08-25 01:13:10 -0300

    Arquivo de introdução adicionado à pagina

--- lib/page/portifolio_page.dart
+++ lib/page/portifolio_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-// import 'package:seu_projeto/sections/section_intro.dart';
-// import 'package:seu_projeto/sections/section_about.dart';
-// import 'package:seu_projeto/sections/section_projects.dart';
-// import 'package:seu_projeto/sections/section_education.dart';
-// import 'package:seu_projeto/sections/section_contact.dart';
+import 'package:portifolio_leonardo_vivo/sections/section_intro.dart';
+// import 'package:portifolio_leonardo_vivo/sections/section_about.dart';
+// import 'package:portifolio_leonardo_vivo/sections/section_projects.dart';
+// import 'package:portifolio_leonardo_vivo/sections/section_education.dart';
+// import 'package:portifolio_leonardo_vivo/sections/section_contact.dart';
 
 class PortifolioPage extends StatelessWidget {
   const PortifolioPage({super.key});
@@ -14,7 +14,7 @@ class PortifolioPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // SectionIntro(),
+            SectionIntro(),
             // SectionAbout(),
             // SectionProjects(),
             // SectionEducation(),
