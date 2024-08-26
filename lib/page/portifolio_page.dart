ad1f1da265913c66188849fd6f9eee3f1e94385b
leonardovivo <leonardovivo3@gmail.com>
2024-08-26 15:02:37 -0300

    Seção de projetos adicionada à página

--- lib/page/portifolio_page.dart
+++ lib/page/portifolio_page.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:portifolio_leonardo_vivo/sections/section_about.dart';
 import 'package:portifolio_leonardo_vivo/sections/section_intro.dart';
+import 'package:portifolio_leonardo_vivo/sections/section_projects.dart';
 
 class PortifolioPage extends StatelessWidget {
   const PortifolioPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             SectionIntro(),
             SectionAbout(),
-            // SectionProjects(),
+            SectionProjects(),
             // SectionSkills(),
             // SectionEducation(),
             // SectionContact(),
