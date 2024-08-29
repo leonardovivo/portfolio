8f6ce67aa44176a4cec3106a746df6d2dbcfb416
leonardovivo <leonardovivo3@gmail.com>
2024-08-29 03:19:09 -0300

    Cards das formações

---
+++ lib/widgets/education_item.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class EducationItem extends StatelessWidget {
+  final IconData icon;
+  final String skillName;
+  final String description;
+  final double height;
+  final double width;
+
+  const EducationItem({
+    super.key,
+    required this.icon,
+    required this.skillName,
+    required this.description,
+    required this.height,
+    required this.width,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.white.withOpacity(0.3)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            blurRadius: 8,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            size: 40,
+            color: Colors.white,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            skillName,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            description,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.white70,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
