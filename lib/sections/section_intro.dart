58c61fb2743e849534d8120c37caae1740f586e8
leonardovivo <leonardovivo3@gmail.com>
2024-08-31 03:54:19 -0300

    Controller do scroll e Stack adicionados

--- lib/sections/section_intro.dart
+++ lib/sections/section_intro.dart
@@ -1,47 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class SectionIntro extends StatelessWidget {
-  const SectionIntro({super.key});
+  final ItemScrollController itemScrollController;
+
+  const SectionIntro({super.key, required this.itemScrollController});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              'Leonardo Vivo Guerreiro',
-              style: TextStyle(
-                fontFamily: 'Dancing Script',
-                fontSize: 100,
-                color: Colors.white,
-              ),
-            )
-            .animate()
-            .fadeIn(duration: 1.seconds)
-            .slide(begin: const Offset(0, 1)),
-
-            const SizedBox(height: 10),
-
-            const Text(
-              'Desenvolvedor Mobile',
-              style: TextStyle(
-                fontFamily: 'Cormorant Garamond',
-                fontSize: 40,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            )
-            .animate()
-            .fadeIn(duration: 1.seconds, delay: 0.5.seconds)
-            .slide(begin: const Offset(0, 1)),
-          ],
+    return Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Leonardo Vivo Guerreiro',
+                  style: TextStyle(
+                    fontFamily: 'Dancing Script',
+                    fontSize: 100,
+                    color: Colors.white,
+                  ),
+                )
+                    .animate()
+                    .fadeIn(duration: 1.seconds)
+                    .slide(begin: const Offset(0, 1)),
+                const SizedBox(height: 10),
+                const Text(
+                  'Desenvolvedor Mobile',
+                  style: TextStyle(
+                    fontFamily: 'Cormorant Garamond',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+                    .animate()
+                    .fadeIn(duration: 1.seconds, delay: 0.5.seconds)
+                    .slide(begin: const Offset(0, 1)),
+              ],
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
