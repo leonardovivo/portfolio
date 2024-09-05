513cb3f0f12ff8e33c034c36727a83025d0bfbb2
leonardovivo <leonardovivo3@gmail.com>
2024-09-05 02:01:49 -0300

    Responsivo para diferentes telas feito e mudança de posição das certificações na tela desktop

--- lib/sections/section_education.dart
+++ lib/sections/section_education.dart
@@ -9,7 +9,16 @@ class SectionEducation extends StatelessWidget {
     return SingleChildScrollView(
       child: LayoutBuilder(
         builder: (context, constraints) {
-          final isMobile = constraints.maxWidth < 600;
+          final screenWidth = constraints.maxWidth;
+
+          final bool isMobile = screenWidth < 600;
+          final bool isTablet = screenWidth >= 600 && screenWidth <= 1024;
+
+          final double paddingHorizontal = isMobile ? 10 : 70;
+          final double fontSizeTitle = isMobile ? 60 : 100;
+          final double cardWidth = isMobile ? double.infinity : 535;
+          final double cardHeight = isMobile ? 250 : 200;
+          final double spacingBetweenCards = isMobile ? 10 : 80;
 
           return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -27,7 +36,7 @@ class SectionEducation extends StatelessWidget {
                             TextSpan(
                               text: 'FORMAÇÃO',
                               style: TextStyle(
-                                fontSize: isMobile ? 60 : 100,
+                                fontSize: fontSizeTitle,
                                 fontWeight: FontWeight.bold,
                                 color: Colors.white.withOpacity(0.1),
                               ),
@@ -35,7 +44,7 @@ class SectionEducation extends StatelessWidget {
                             TextSpan(
                               text: '.',
                               style: TextStyle(
-                                fontSize: isMobile ? 60 : 100,
+                                fontSize: fontSizeTitle,
                                 fontWeight: FontWeight.bold,
                                 color: const Color.fromARGB(255, 145, 0, 255),
                               ),
@@ -49,49 +58,146 @@ class SectionEducation extends StatelessWidget {
               ),
               const SizedBox(height: 100),
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: isMobile ? 10 : 70),
-                child: Wrap(
-                  spacing: isMobile ? 10 : 80,
-                  runSpacing: 20,
-                  children: [
-                    SizedBox(
-                      width: isMobile ? double.infinity : 535,
-                      child: CardsCertificates(
-                        typeCertificate: 'Tecnólogo',
-                        certificateName:
-                            'Análise e Desenvolvimento de Sistemas',
-                        institution: 'FATEC Ipiranga - Pastor Enéas Tognini',
-                        period: '2023 - 2025',
-                        height: isMobile ? 250 : 200,
-                        width: isMobile ? double.infinity : 535,
-                      ),
-                    ),
-                    SizedBox(
-                      width: isMobile ? double.infinity : 535,
-                      child: CardsCertificates(
-                        typeCertificate: 'Formação Flutter',
-                        certificateName:
-                            'Desenvolva seu primeiro app com Flutter',
-                        institution: 'Alura',
-                        period: '96h - 2023',
-                        height: isMobile ? 250 : 200,
-                        width: isMobile ? double.infinity : 535,
-                      ),
-                    ),
-                    SizedBox(
-                      width: isMobile ? double.infinity : 535,
-                      child: CardsCertificates(
-                        typeCertificate: 'Formação Dart',
-                        certificateName:
-                            'Crie projetos em Dart, a linguagem utilizada no Flutter',
-                        institution: 'Alura',
-                        period: '55h - 2023',
-                        height: isMobile ? 250 : 200,
-                        width: isMobile ? double.infinity : 535,
-                      ),
-                    ),
-                  ],
-                ),
+                padding: EdgeInsets.symmetric(horizontal: paddingHorizontal),
+                child: isMobile
+                    ? Wrap(
+                        spacing: spacingBetweenCards,
+                        runSpacing: 20,
+                        children: [
+                          SizedBox(
+                            width: cardWidth,
+                            child: CardsCertificates(
+                              typeCertificate: 'Tecnólogo',
+                              certificateName:
+                                  'Análise e Desenvolvimento de Sistemas',
+                              institution:
+                                  'FATEC Ipiranga - Pastor Enéas Tognini',
+                              period: '2023 - 2025',
+                              height: cardHeight,
+                              width: cardWidth,
+                            ),
+                          ),
+                          SizedBox(
+                            width: cardWidth,
+                            child: CardsCertificates(
+                              typeCertificate: 'Formação Flutter',
+                              certificateName:
+                                  'Desenvolva seu primeiro app com Flutter',
+                              institution: 'Alura',
+                              period: '96h - 2023',
+                              height: cardHeight,
+                              width: cardWidth,
+                            ),
+                          ),
+                          SizedBox(
+                            width: cardWidth,
+                            child: CardsCertificates(
+                              typeCertificate: 'Formação Dart',
+                              certificateName:
+                                  'Crie projetos em Dart, a linguagem utilizada no Flutter',
+                              institution: 'Alura',
+                              period: '55h - 2023',
+                              height: cardHeight,
+                              width: cardWidth,
+                            ),
+                          ),
+                        ],
+                      )
+                    : isTablet
+                        ? Column(
+                            children: [
+                              SizedBox(
+                                width: cardWidth,
+                                child: CardsCertificates(
+                                  typeCertificate: 'Tecnólogo',
+                                  certificateName:
+                                      'Análise e Desenvolvimento de Sistemas',
+                                  institution:
+                                      'FATEC Ipiranga - Pastor Enéas Tognini',
+                                  period: '2023 - 2025',
+                                  height: cardHeight,
+                                  width: cardWidth,
+                                ),
+                              ),
+                              const SizedBox(height: 40),
+                              SizedBox(
+                                width: cardWidth,
+                                child: CardsCertificates(
+                                  typeCertificate: 'Formação Flutter',
+                                  certificateName:
+                                      'Desenvolva seu primeiro app com Flutter',
+                                  institution: 'Alura',
+                                  period: '96h - 2023',
+                                  height: cardHeight,
+                                  width: cardWidth,
+                                ),
+                              ),
+                              const SizedBox(height: 40),
+                              SizedBox(
+                                width: cardWidth,
+                                child: CardsCertificates(
+                                  typeCertificate: 'Formação Dart',
+                                  certificateName:
+                                      'Crie projetos em Dart, a linguagem utilizada no Flutter',
+                                  institution: 'Alura',
+                                  period: '55h - 2023',
+                                  height: cardHeight,
+                                  width: cardWidth,
+                                ),
+                              ),
+                            ],
+                          )
+                        : Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  SizedBox(
+                                    width: cardWidth,
+                                    child: CardsCertificates(
+                                      typeCertificate: 'Tecnólogo',
+                                      certificateName:
+                                          'Análise e Desenvolvimento de Sistemas',
+                                      institution:
+                                          'FATEC Ipiranga - Pastor Enéas Tognini',
+                                      period: '2023 - 2025',
+                                      height: cardHeight,
+                                      width: cardWidth,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: cardWidth,
+                                    child: CardsCertificates(
+                                      typeCertificate: 'Formação Flutter',
+                                      certificateName:
+                                          'Desenvolva seu primeiro app com Flutter',
+                                      institution: 'Alura',
+                                      period: '96h - 2023',
+                                      height: cardHeight,
+                                      width: cardWidth,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 40),
+                              Align(
+                                alignment: Alignment.center,
+                                child: SizedBox(
+                                  width: cardWidth,
+                                  child: CardsCertificates(
+                                    typeCertificate: 'Formação Dart',
+                                    certificateName:
+                                        'Crie projetos em Dart, a linguagem utilizada no Flutter',
+                                    institution: 'Alura',
+                                    period: '55h - 2023',
+                                    height: cardHeight,
+                                    width: cardWidth,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
               ),
             ],
           );
